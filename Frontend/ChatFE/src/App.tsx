18d2c441975a04a75f2076574b72bb03511353e5
Alok Yadav <alokjar212@gmail.com>
2026-02-22 02:09:20 +0530

    FE setup done initial commit

---
+++ Frontend/ChatFE/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='bg-zinc-800 text-white text-center'>
+        <h1>Chat App</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
