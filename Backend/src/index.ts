7ccd1a81c16a9594a1f7144d909e9658180c6d61
Alok Yadav <alokjar212@gmail.com>
2026-02-22 00:24:12 +0530

    Server setup done using ws library

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+let allSockets = [];
+
+wss.on("connection", (socket) => {
+
+    allSockets.push(socket);
+    userCount++;
+    console.log(`User connected # ${userCount}`);
+
+    socket.on("message", (message) => {
+        console.log(`message received ${message.toString()}`);
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s?.send(`${message.toString()} Sent from server`);
+        }
+    })
+});
+
